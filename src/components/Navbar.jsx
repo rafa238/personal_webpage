8f664f2d73fde7a3fe4c86e955802959717cdce8
rafa238 <rafalaureano246@gmail.com>
2023-08-05 16:18:22 -0600

    first version navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,31 @@ import { DataContext } from '../context/DataProvider'
 export const Navbar = () => {
   const {language, setLanguage} = useContext(DataContext);
   return (
-    <nav>
-      <ul className='main-nav'>
-        <li className='main-nav__item'>
-          <a href='#' > Home </a>
-        </li>
-        <li className='main-nav__item'>
-          <a href='#' > About </a>
-        </li>
-        <li className='main-nav__item'>
-          <a href='#' > My projects </a>
-        </li>
-      </ul>
-
-      <input 
-        type='checkbox' 
-        value="language"
-        checked={language}
-        onChange={() => setLanguage(!language)}/>
+    <nav className='nav'>
+      <div className='nav__left'>
+        <span className='nav-name'>
+          <strong>Rafa</strong> JL
+        </span>
+        <ul className='main-nav'>
+          <li className='main-nav__item'>
+            <a href='#' > Home </a>
+          </li>
+          <li className='main-nav__item'>
+            <a href='#' > About </a>
+          </li>
+          <li className='main-nav__item'>
+            <a href='#' > My projects </a>
+          </li>
+        </ul>
+      </div>
+      <label class="nav__switchBtn">
+        <input 
+          type='checkbox' 
+          checked={language}
+          onChange={() => setLanguage(!language)}/>
+          <div class="slide round">spanish</div>
+      </label>
+      
     </nav>
   )
 }
